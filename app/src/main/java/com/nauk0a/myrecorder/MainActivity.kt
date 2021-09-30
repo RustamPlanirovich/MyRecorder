9075641c907c7b26f5933730958df7523cf7b803
RustamPlanirovich <israr.ru@yandex.ru>
2021-09-30 18:55:52 +0700

    - create project - добавление основных экранов - создание файла навигационного графа

---
+++ app/src/main/java/com/nauk0a/myrecorder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nauk0a.myrecorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
