3fa379d19d1084b21f70b5795be5c507967bc34d
RustamPlanirovich <israr.ru@yandex.ru>
2021-09-30 19:42:04 +0700

    - добавил dataBinding - доделал меню нижнего бара - код в MainActivity.kt для того чтобы навигация работала

--- app/src/main/java/com/nauk0a/myrecorder/MainActivity.kt
+++ app/src/main/java/com/nauk0a/myrecorder/MainActivity.kt
@@ -2,10 +2,24 @@ package com.nauk0a.myrecorder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.nauk0a.myrecorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigation,
+            Navigation.findNavController(this, R.id.nav_host_fragment_container)
+        )
     }
 }
